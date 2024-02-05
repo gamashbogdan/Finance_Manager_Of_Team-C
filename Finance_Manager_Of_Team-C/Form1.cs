a9df4b67febbe3919f470010bf882983ba63d38b
gamashbogdan <gamashbogdan@gmail.com>
2024-02-05 19:20:10 +0100

    Add project files.

---
+++ Finance_Manager_Of_Team-C/Form1.cs
@@ -0,0 +1,10 @@
+namespace Finance_Manager_Of_Team_C
+{
+    public partial class FormFinance : Form
+    {
+        public FormFinance()
+        {
+            InitializeComponent();
+        }
+    }
+}
