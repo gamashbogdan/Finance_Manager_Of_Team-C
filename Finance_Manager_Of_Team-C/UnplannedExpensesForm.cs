357194bbb5e1d8683b524bae845a672d1e2feb1f
gamashbogdan <gamashbogdan@gmail.com>
2024-02-06 20:28:54 +0100

    3 - Створив форми для майбутніх вкладок витрат

---
+++ Finance_Manager_Of_Team-C/UnplannedExpensesForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Finance_Manager_Of_Team_C
+{
+    public partial class UnplannedExpensesForm : Form
+    {
+        public UnplannedExpensesForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
