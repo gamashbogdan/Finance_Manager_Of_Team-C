fec3966b1dac00076c7ca7585677ba2ba038f08e
gamashbogdan <gamashbogdan@gmail.com>
2024-02-08 19:51:23 +0100

    5 - Виправив помилки в коді

--- Finance_Manager_Of_Team-C/UnplannedExpensesForm.cs
+++ Finance_Manager_Of_Team-C/UnplannedExpensesForm.cs
@@ -10,63 +10,47 @@ using System.Windows.Forms;
  
  
 namespace Finance_Manager_Of_Team_C 
-{ 
-    public partial class UnplannedExpensesForm : Form 
-    { 
+{
+    public partial class UnplannedExpensesForm : Form
+    {
         private TextBox categoryTextBox;
-        private TextBox descriptionTextBox; 
-        private TextBox amountTextBox; 
- 
-        private List<Expense> unplannedExpenses = new List<Expense>(); 
- 
-        public UnplannedExpensesForm() 
-        { 
-            InitializeComponent(); 
-        } 
- 
-        private void InitializeComponent() 
-        { 
-           
- 
-            this.categoryTextBox = new System.Windows.Forms.TextBox(); 
-            this.descriptionTextBox = new System.Windows.Forms.TextBox(); 
-            this.amountTextBox = new System.Windows.Forms.TextBox(); 
- 
-           
- 
-           
-            this.Controls.Add(this.categoryTextBox); 
-            this.Controls.Add(this.descriptionTextBox); 
-            this.Controls.Add(this.amountTextBox); 
- 
-          
-        } 
- 
-        private void addButton_Click(object sender, EventArgs e) 
-        { 
-            string category = categoryTextBox.Text; 
-            string description = descriptionTextBox.Text; 
-            decimal amount; 
- 
-            if (decimal.TryParse(amountTextBox.Text, out amount)) 
-            { 
-                Expense newExpense = new Expense(category, description, amount); 
-                unplannedExpenses.Add(newExpense); 
- 
-               
-                categoryTextBox.Clear(); 
-                descriptionTextBox.Clear(); 
-                amountTextBox.Clear(); 
-            } 
-            else 
-            { 
-                MessageBox.Show("Некоректна сума витрат. Будь ласка, введіть числове значення."); 
-            } 
-        } 
- 
-     
-    } 
- 
+        private TextBox descriptionTextBox;
+        private TextBox amountTextBox;
+
+        private List<Expense> unplannedExpenses = new List<Expense>();
+
+        public UnplannedExpensesForm()
+        {
+            InitializeComponent();
+        }
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string category = categoryTextBox.Text;
+            string description = descriptionTextBox.Text;
+            decimal amount;
+
+            if (decimal.TryParse(amountTextBox.Text, out amount))
+            {
+                Expense newExpense = new Expense(category, description, amount);
+                unplannedExpenses.Add(newExpense);
+
+
+                categoryTextBox.Clear();
+                descriptionTextBox.Clear();
+                amountTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Некоректна сума витрат. Будь ласка, введіть числове значення.");
+            }
+        }
+
+        private void buttonDomesticExpenses_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
     public class Expense 
     { 
         public string Category { get; } 
